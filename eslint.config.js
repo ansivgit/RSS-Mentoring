55c949e76a8052c0e425dbb4ddaf0874142d206a
Erika Milevskaya <95300271+rika-milew@users.noreply.github.com>
2026-01-14 22:03:45 +0300

    chore: add unicorn/number-literal-case rule (#3)
    * Disable unicorn/number-literal-case rule
    
    * fix: indent
    
    ---------
    
    Co-authored-by: ansivgit <ansiv@yandex.ru>

--- eslint.config.js
+++ eslint.config.js
@@ -124,11 +124,11 @@ export default defineConfig([
       'unicorn/no-array-reduce': 'off',
       'unicorn/no-array-for-each': 'off',
       'unicorn/filename-case': 'off',
+      'unicorn/number-literal-case': 'off',
       'unicorn/prefer-query-selector': 'warn',
       '@typescript-eslint/no-misused-promises': 'off', //* switched off for now
       '@typescript-eslint/restrict-template-expressions': 'off', //* switched off for now
       '@typescript-eslint/no-inferrable-types': 'error',
-
     },
   },
   {
